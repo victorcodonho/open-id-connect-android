2016cb7b85392cafe1aad9d11518c74ce5774cf9
Victor Codonho <victor_codonho@outlook.com>
2026-06-23 15:27:17 -0300

    feat: added lib navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.navigation.compose)
     implementation(libs.net.openid.appauth)
     implementation(libs.com.google.dagger.hilt.android)
     implementation(libs.com.squareup.retrofit2)
