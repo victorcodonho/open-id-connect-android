00af354fdf94157a4f5920fca34835363ec971ed
Victor Codonho <victor_codonho@outlook.com>
2026-06-23 15:20:15 -0300

    feat: added libs and config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,13 +48,18 @@ android {
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.datastore.preferences)
     implementation(libs.net.openid.appauth)
+    implementation(libs.com.google.dagger.hilt.android)
+    implementation(libs.com.squareup.retrofit2)
+    implementation(libs.com.squareup.logging.interceptor)
 
     testImplementation(libs.junit)
 
